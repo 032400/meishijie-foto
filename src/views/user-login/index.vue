ee67a5c098d5c574dda906bf441c264333083fd7
MrsYu <ymy_0213@163.com>
2021-11-03 17:54:50 +0800

    修改登录按钮的text

--- src/views/user-login/index.vue
+++ src/views/user-login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-page">
 
     <el-tabs v-model="activeName" type="card">
-      <el-tab-pane label="登录" name="login">
+      <el-tab-pane label="点我登录" name="login">
         <login></login>
       </el-tab-pane>
       <el-tab-pane label="注册" name="second">
